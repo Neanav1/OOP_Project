704b1ca4653bc7afea8c3e6a02acd85258336f28
hayato612 <haferh@blundells.org>
2026-05-01 14:47:53 +0100

    player loop added

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ player = Knight(10)
 enemy = Knight(10)
 
 while player.get_hp() > 0 or enemy.get_hp() >0  :
+    
     player.greet()
     player.get_hp()
-    
+    player.get_actions()
+    while player.get_actions() > 0:
+        #loop for player action, ends when player has no more actions
+        command_player = input("What would you like to do? ('1' for attack, '2' for heal, '3' for turn) ")
+        
+        
+
+
+
+
+
+
 
