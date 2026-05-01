8980af0953bbee528f18125e10b929ba938e6a2a
hayato612 <haferh@blundells.org>
2026-05-01 14:55:21 +0100

    player trun loop

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ enemy = Knight(10)
 while player.get_hp() > 0 or enemy.get_hp() >0  :
     
     player.greet()
-    player.get_hp()
-    player.get_actions()
     while player.get_actions() > 0:
+        print("you have 2 dices") #need function to show number of dices player got 
+        dices = input(int("How many dices would you like to use: "))
+        player.roll_dices(dices)
         #loop for player action, ends when player has no more actions
         command_player = input("What would you like to do? ('1' for attack, '2' for heal, '3' for turn) ")
         
