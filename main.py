4d3f651effee1ac939f9fa8278316a064d2dbf87
hayato612 <haferh@blundells.org>
2026-05-05 10:35:43 +0100

    player turn

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def player_turn(enemy,player):
     while player.get_action_points() > 0:
         dices = int(input("How many dices would you like to use: "))
         player.roll_dices(dices)
+        player.greet()
         #loop for player action, ends when player has no more actions
         print(player.get_available_actions)
         
@@ -16,9 +17,6 @@ def player_turn(enemy,player):
 
 player = Knight(10)
 enemy = Knight(10)
-
-#this will do the ai_turns
-enemy_turn(enemy,player)
 
 
 while player.get_hp() > 0 or enemy.get_hp() >0  :
